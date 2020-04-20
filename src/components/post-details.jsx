1632d7428a82ed805e9656b7abf0ebb53802a9dc
Hovhannes98 <Hovhannes98@gmail.com>
2020-04-20 10:29:10 +0400

    selected post

--- src/components/post-details.jsx
+++ src/components/post-details.jsx
@@ -1,23 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 import { Jumbotron, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { fetchPosts } from "../functions/post-functions";
 
-const PostDetails = () => {
-  return (
-    <div className="post-item-details">
-      <Jumbotron>
-        <h1>hgjghjghj</h1>
-        <p>jhkhjkhjk</p>
-        <Link to="/posts">
-          <p>
-            <Button className="close-post-btn" variant="secondary">
-              Close Post
-            </Button>
-          </p>
-        </Link>
-      </Jumbotron>
-    </div>
-  );
-};
+class PostDetails extends Component {
+  state = {
+    selectedPost: {},
+  };
+
+  componentDidMount = () => {
+    fetchPosts().then((posts) => {
+      const selectedPost = posts.filter(
+        (post) => post.id === this.props.match.params.id
+      );
+      this.setState({ selectedPost: selectedPost[0] });
+    });
+  };
+
+  render() {
+    return (
+      <div className="post-item-details">
+        <Jumbotron>
+          <h1 className="post-details-title">
+            {this.state.selectedPost.title}
+          </h1>
+          <p>{this.state.selectedPost.description}</p>
+          <Link to="/posts">
+            <p>
+              <Button className="close-post-btn" variant="secondary">
+                Close Post
+              </Button>
+            </p>
+          </Link>
+        </Jumbotron>
+      </div>
+    );
+  }
+}
 
 export default PostDetails;
