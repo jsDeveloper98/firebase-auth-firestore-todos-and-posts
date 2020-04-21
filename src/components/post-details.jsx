490054c0f62bd6fa1188aa820db2338dda9d5ab9
Hovhannes98 <Hovhannes98@gmail.com>
2020-04-21 14:00:37 +0400

    selected-post

--- src/components/post-details.jsx
+++ src/components/post-details.jsx
@@ -1,41 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import { Jumbotron, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { fetchPosts } from "../functions/post-functions";
 
-class PostDetails extends Component {
-  state = {
-    selectedPost: {},
-  };
+const PostDetails = (props) => {
+  const { selectedPost } = props.location.state;
 
-  componentDidMount = () => {
-    fetchPosts().then((posts) => {
-      const selectedPost = posts.filter(
-        (post) => post.id === this.props.match.params.id
-      );
-      this.setState({ selectedPost: selectedPost[0] });
-    });
-  };
-
-  render() {
-    return (
-      <div className="post-item-details">
-        <Jumbotron>
-          <h1 className="post-details-title">
-            {this.state.selectedPost.title}
-          </h1>
-          <p>{this.state.selectedPost.description}</p>
-          <Link to="/posts">
-            <p>
-              <Button className="close-post-btn" variant="secondary">
-                Close Post
-              </Button>
-            </p>
-          </Link>
-        </Jumbotron>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="post-item-details">
+      <Jumbotron>
+        <h1 className="post-details-title">{selectedPost.title}</h1>
+        <p>{selectedPost.description}</p>
+        <Link to="/posts">
+          <Button className="close-post-btn" variant="secondary">
+            Close Post
+          </Button>
+        </Link>
+      </Jumbotron>
+    </div>
+  );
+};
 
 export default PostDetails;
--- src/components/post.jsx
+++ src/components/post.jsx
@@ -7,11 +7,17 @@ const Post = (props) => {
     <Jumbotron>
       <h1 className="post-title">{props.post.title}</h1>
       <p className="post-decription">{props.post.description}</p>
-      <Link to={"/post/" + props.post.id}>
-        <p>
-          <Button variant="primary">Open Post</Button>
-        </p>
+      <Link
+        to={{
+          pathname: "/post/" + props.post.id,
+          state: {
+            selectedPost: props.post,
+          },
+        }}
+      >
+        <Button variant="primary">Open Post</Button>
       </Link>
+
       <div className="remove-post" onClick={() => props.onRemove(props.post)}>
         ✗
       </div>
