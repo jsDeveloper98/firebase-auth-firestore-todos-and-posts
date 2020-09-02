8eb441f9d5e518216e3316603e0c66f4ff314a32
Hovhannes98 <Hovhannes98@gmail.com>
2020-09-02 17:24:03 +0400

    installing redux

--- src/index.js
+++ src/index.js
@@ -4,10 +4,19 @@ import "./index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { createStore } from "redux";
+import { rootReducer } from "./redux/root-reducer";
+import { Provider } from "react-redux";
+
+const store = createStore(rootReducer);
+
+console.log({ store });
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/redux/posts-reducer.jsx
@@ -0,0 +1,8 @@
+const initialState = {
+  posts: [],
+  getchedPosts: []
+}
+
+export const postsReducer = (state = initialState, action) => {
+  return state;
+}
